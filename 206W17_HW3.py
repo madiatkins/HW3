63ddfb2ca9cfe547549f10ec1711d237361d5ab4
Madi Atkins <madiatk@umich.edu>
2017-01-31 15:16:19 -0500

    Finished last pieces of Part2

--- 206W17_HW3.py
+++ 206W17_HW3.py
@@ -71,14 +71,33 @@ for line in file:
         linenum2.append(line)
 
 
+full_paths_num = len(linenum2)
 ## (c) Write Python code to determine how many of these paths describe a Python file saved
 # inside a folder called SI206. 
 #Save that number in the variable python_course_paths.
+file = open('computer_paths.txt')
+linenum3 = []
+for line in file:
+    line = line.rstrip()
+    if re.findall("SI206/.+[.]py", line):
+        linenum3.append(line)
 
-
+python_course_paths = len(linenum3)
 ## (d) Write Python code to determine how many of these paths describe a Microsoft file 
 #(a file that EITHER ends with .docx OR .xlsx, but nothing else counts) where the file name ends in a digit. 
 #Save that total in the variable microsoft_files_num.
+
+
+file = open('computer_paths.txt')
+linenum4 = []
+for line in file:
+    line = line.rstrip()
+    if re.findall("[0-9]+[.]", line):
+        if re.findall("[.].+[x]", line):
+            print(line) 
+            linenum4.append(line)
+
+microsoft_files_num = len(linenum4)
 
 
 
